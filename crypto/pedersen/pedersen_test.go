745bc5946fffc6e6ae3824df0972530309988884
Marcello Bardus <marcello@bardus.it>
2022-07-07 15:33:14 +0100

    pedersen additional tests

--- crypto/pedersen/pedersen_test.go
+++ crypto/pedersen/pedersen_test.go
@@ -1,6 +1,7 @@
 package pedersen_test
 
 import (
+	"encoding/binary"
 	"math/big"
 	"testing"
 
@@ -11,7 +12,20 @@ import (
 
 // the purpose of this test is primarily to ensure that the randomness
 // generation won't error.
-func TestPedersen(t *testing.T) {
+func TestPedersenSimpleDigest(t *testing.T) {
 	result := pedersen.Digest(big.NewInt(44), big.NewInt(0))
 	require.Equal(t, result.Cmp(big.NewInt(0)), 1)
+}
+
+func TestPedersenArrayDigest(t *testing.T) {
+	result := pedersen.ArrayDigest(big.NewInt(44), big.NewInt(44), big.NewInt(44), big.NewInt(44))
+	require.Equal(t, result.Cmp(big.NewInt(0)), 1)
+}
+
+func TestPedersenHash(t *testing.T) {
+	hasher := pedersen.New()
+	hasher.Write([]byte{10, 10, 10})
+	result := hasher.Sum(nil)
+	resultInt := binary.BigEndian.Uint64(result)
+	require.Greater(t, resultInt, uint64(0))
 }
