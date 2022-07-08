8e723102297527bcfbe449c0ea30b52cb0a981cd
Marcello Bardus <marcello@bardus.it>
2022-07-08 16:34:05 +0100

    stark signature abstraction rework

---
+++ crypto/stark/stark_test.go
@@ -0,0 +1,21 @@
+package stark_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/crypto/stark"
+)
+
+func TestGenPrivKey(t *testing.T) {
+	pv := stark.GenPrivKey()
+	require.Equal(t, len(pv.Bytes()), 32)
+}
+
+func TestSign(t *testing.T) {
+	pv := stark.GenPrivKey()
+
+	msg := []byte("hello world")
+	sig, _ := pv.Sign(msg)
+	require.Equal(t, len(sig), 64)
+}
