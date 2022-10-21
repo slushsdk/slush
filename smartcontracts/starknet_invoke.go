81c7eca2ca17749988d13e152c79c248ce14db51
Bence Haromi <bence.haromi@gmail.com>
2022-10-21 21:12:27 +0100

    invoke updated

--- smartcontracts/starknet_invoke.go
+++ smartcontracts/starknet_invoke.go
@@ -10,7 +10,7 @@ import (
 func Invoke(types.VerifierDetails) ([]byte, error) {
 
 	//Declare cairo contract
-	cmd := exec.Command("protostar", "migrate", "migrations/migration_01.cairo", "--network", "alpha-goerli", "--private-key-path", "./pkey", "--account-address", "0x07e0e42703bE10f32F8c793395C3713141C15a3A80FF18e7515Df194DaC3eea7", "--output-dir", "responses", "--no-confirm")
+	cmd := exec.Command("protostar", "migrate", "migrations/migration_01.cairo", "--network", "alpha-goerli", "--private-key-path", "./pkey", "--account-address", "0x07e0e42703bE10f32F8c793395C3713141C15a3A80FF18e7515Df194DaC3eea7", "--output-dir", "migrations/output", "--no-confirm")
 	cmd.Dir = "../../tendermint-cairo"
 
 	stdout, err := cmd.CombinedOutput()
