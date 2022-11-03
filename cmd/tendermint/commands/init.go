4ce637d7835bf66c37ae3f280dda0fc77c15a563
Kalman <lajkokala@yahoo.com>
2022-11-03 19:17:27 +0000

    feat(cmd/tendermint/commands/init.go): changed init to deploy the contracts to L2  SLU-19

--- cmd/tendermint/commands/init.go
+++ cmd/tendermint/commands/init.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"regexp"
 
 	"github.com/spf13/cobra"
 
@@ -13,12 +14,17 @@ import (
 	tmrand "github.com/tendermint/tendermint/libs/rand"
 	tmtime "github.com/tendermint/tendermint/libs/time"
 	"github.com/tendermint/tendermint/privval"
+	"github.com/tendermint/tendermint/smartcontracts"
 	"github.com/tendermint/tendermint/types"
 )
 
 // MakeInitFilesCommand returns the command to initialize a fresh Tendermint Core instance.
 func MakeInitFilesCommand(conf *config.Config, logger log.Logger) *cobra.Command {
 	var keyType string
+	var pathToFiles string
+	var network string
+	var devnetbool string
+
 	cmd := &cobra.Command{
 		Use:       "init [full|validator|seed]",
 		Short:     "Initializes a Tendermint node",
@@ -30,12 +36,44 @@ func MakeInitFilesCommand(conf *config.Config, logger log.Logger) *cobra.Command
 				return errors.New("must specify a node type: tendermint init [validator|full|seed]")
 			}
 			conf.Mode = args[0]
+
+			var b bool
+			if devnetbool == "1" {
+				b = true
+			} else {
+				b = false
+
+			}
+			ret, err := smartcontracts.DeclareDeploy(pathToFiles, network, b)
+
+			if err != nil {
+				fmt.Println(string(ret))
+				fmt.Println(err)
+				return err
+			}
+			logger.Info("Successfully declared and deployed contract at:...")
+			line := regexp.MustCompile(`contract_address *.*?\n`)
+			zerox := regexp.MustCompile(`0x.{64}`)
+			fmt.Printf("%q\n", (line.Find(ret)))
+			fmt.Printf("%q\n", zerox.Find(line.Find(ret)))
+
 			return initFilesWithConfig(cmd.Context(), conf, logger, keyType)
+
 		},
 	}
 
 	cmd.Flags().StringVar(&keyType, "key", types.ABCIPubKeyTypeStark,
 		"Key type to generate privval file with. Options: stark, ed25519, secp256k1")
+
+	cmd.Flags().StringVar(&pathToFiles, "path-to-files", "",
+		"For mainnet or testnet: relative path to folder storing wallet account's private key file and address file, stored as pkey and address stored as hex without leading 0x. ")
+
+	cmd.Flags().StringVar(&network, "network", "alpha-goerli",
+		"Network to deploy on: alpha-mainnet, alpha-goerli, or devnet (assumed at http://127.0.0.1:5050). If using devnet either provide keys, or launch devnet using seed=42.")
+	cmd.MarkFlagRequired("network")
+
+	cmd.Flags().StringVar(&devnetbool, "devnetbool", "1",
+		"If using devnet either provide keys (default), or launch devnet using seed=42 and set --devnetbool=1.")
 
 	return cmd
 }
