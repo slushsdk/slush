bab8ab1b3ba2f33bcfd7236375f75ed2633406fb
Bence Haromi <bence.haromi@gmail.com>
2022-11-10 12:16:36 +0000

    test(message_test): TestBlockchainMessageVectors expected hash updated SLU-12

--- proto/tendermint/blocksync/message_test.go
+++ proto/tendermint/blocksync/message_test.go
@@ -102,7 +102,7 @@ func TestBlockchainMessageVectors(t *testing.T) {
 			BlockRequest: &bcproto.BlockRequest{Height: math.MaxInt64}}},
 			"0a0a08ffffffffffffffff7f"},
 		{"BlockResponseMessage", &bcproto.Message{Sum: &bcproto.Message_BlockResponse{
-			BlockResponse: &bcproto.BlockResponse{Block: bpb}}}, "1a700a6e0a5b0a02080b1803220b088092b8c398feffffff012a0212003a20c4da88e876062aa1543400d50d0eaa0dac88096057949cfb7bca7f3a48c04bf96a20e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855120d0a0b48656c6c6f20576f726c641a00"},
+			BlockResponse: &bcproto.BlockResponse{Block: bpb}}}, "1a700a6e0a5b0a02080b1803220b088092b8c398feffffff012a0212003a20038f7b22befb72426e89b4fddaa0f6aba6063bbb9d6b259f8b533a6256389b906a20049ee3eba8c1600700ee1b87eb599f16716b0b1022947733551fde4050ca6804120d0a0b48656c6c6f20576f726c641a00"},
 		{"NoBlockResponseMessage", &bcproto.Message{Sum: &bcproto.Message_NoBlockResponse{
 			NoBlockResponse: &bcproto.NoBlockResponse{Height: 1}}}, "12020801"},
 		{"NoBlockResponseMessage", &bcproto.Message{Sum: &bcproto.Message_NoBlockResponse{
