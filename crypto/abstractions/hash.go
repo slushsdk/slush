417b9b6bf0c080bcfc3f065d57d8f743fbb1ccc1
Kalman Lajko <lajkokala@yahoo.com>
2022-11-29 16:29:16 +0000

    finished serialisation replacement, testing left
    (cherry picked from commit fcb847397b9e7b1054b7ce3d58d9238efc809c76)

--- crypto/abstractions/hash.go
+++ crypto/abstractions/hash.go
@@ -1,6 +1,7 @@
 package abstractions
 
 import (
+	"bytes"
 	sha256 "crypto/sha256"
 	"hash"
 )
@@ -14,4 +15,12 @@ func New() hash.Hash {
 func Sum256(b []byte) [32]byte {
 	r := sha256.Sum256(b)
 	return r
+}
+
+//We want to pass in 64 bit numbers to pedersen, so we want to round the byte array to be that long.
+func ByteRounder(ba []byte) []byte {
+
+	rem := len(ba) % 8
+	return bytes.Join([][]byte{ba, make([]byte, rem)}, make([]byte, 0))
+
 }
--- types/evidence_test.go
+++ types/evidence_test.go
@@ -378,7 +378,7 @@ func TestEvidenceVectors(t *testing.T) {
 		},
 		{"LightClientAttackEvidence",
 			EvidenceList{lcae},
-			"2f8782163c3905b26e65823ababc977fe54e97b94e60c0360b1e4726b668bb8e",
+			"2bb828ba291fbce378d8834883c477a95a94fdab690dd5b80b4c0136fa69375a",
 		},
 		{"LightClientAttackEvidence & DuplicateVoteEvidence",
 			EvidenceList{&DuplicateVoteEvidence{VoteA: v2, VoteB: v}, lcae},
