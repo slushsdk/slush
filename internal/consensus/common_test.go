9d185ef9b744c923cd1006ae01efd22c1a11f753
Kalman Lajko <lajkokala@yahoo.com>
2022-11-30 10:52:51 +0000

    debugging concurrency error
    (cherry picked from commit 80185b42dacfe11105646b90bfdac863160fd242)

--- internal/consensus/common_test.go
+++ internal/consensus/common_test.go
@@ -40,7 +40,7 @@ const (
 	testSubscriber = "test-client"
 
 	// genesis, chain_id, priv_val
-	ensureTimeout = time.Millisecond * 200
+	ensureTimeout = 10 * time.Millisecond * 200
 )
 
 // A cleanupFunc cleans up any config / test files created for a particular
