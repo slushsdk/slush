e8e00840dd1cdb267b6433e981f295ee88999c7e
Bence Haromi <bence.haromi@gmail.com>
2022-12-21 13:15:20 +0100

    feat(protostar): Deploy function with tests SLU-129

--- protostar/protostar_test.go
+++ protostar/protostar_test.go
@@ -295,6 +295,65 @@ func TestGetTransactionHashHex(t *testing.T) {
 	}
 }
 
+func TestGetTransactionHashFelt(t *testing.T) {
+	testCases := []struct {
+		name     string
+		input    []byte
+		expected string
+		error    error
+	}{
+		0: {
+			name:  "empty input",
+			input: []byte(""),
+			error: fmt.Errorf("could not find transaction hash felt in stdout: %s", ""),
+		},
+		1: {
+			name:  "invalid one-line input",
+			input: []byte("invalid input"),
+			error: fmt.Errorf("could not find transaction hash felt in stdout: %s", "invalid input"),
+		},
+		2: {
+			name:     "valid one-line input",
+			input:    []byte("Transaction hash: 1234567890"),
+			expected: "1234567890",
+		},
+		3: {
+			name:     "valid multi-line input",
+			input:    []byte("Example first line,\nsecond line with some hash: 0x9876543210\nTransaction hash: 1234567890\nLast line with some hash: 9459459459"),
+			expected: "1234567890",
+		},
+		4: {
+			name:  "invalid multi-line input",
+			input: []byte("Example first line,\nsecond line with some hash: 0x9876543210\nhash: 0x1234567890\nLast line with some hash: 0x9459459459\nhash: 0x1234567890"),
+			error: fmt.Errorf("could not find transaction hash felt in stdout: %s", "Example first line,\nsecond line with some hash: 0x9876543210\nhash: 0x1234567890\nLast line with some hash: 0x9459459459\nhash: 0x1234567890"),
+		},
+		5: {
+			name:  "invalid input with 0x prefix",
+			input: []byte("Transaction hash: 0x1234567890"),
+			error: fmt.Errorf("could not find transaction hash felt in stdout: %s", "Transaction hash: 0x1234567890"),
+		},
+		6: {
+			name:  "invalid input without 0x prefix and invalid integer",
+			input: []byte("Transaction hash: 12345OPW78Z9CV"),
+			error: fmt.Errorf("could not find transaction hash felt in stdout: %s", "Transaction hash: 12345OPW78Z9CV"),
+		},
+	}
+	for i, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getTransactionHashFelt(tt.input)
+			if err != nil {
+				if tt.error == nil || err.Error() != tt.error.Error() {
+					t.Errorf("testCase%d failed: getTransactionHash() error = %v, wantErr %v", i, err, tt.error)
+				}
+				return
+			}
+			if got != tt.expected {
+				t.Errorf("testCase%d failed: getTransactionHash() = %v, want %v", i, got, tt.expected)
+			}
+		})
+	}
+}
+
 func TestGetContractAddress(t *testing.T) {
 	testCases := []struct {
 		name     string
@@ -388,5 +447,48 @@ func TestDeclare(t *testing.T) {
 	classHashHex, transactionHashHex, err := Declare(conf, "../cairo/build/main.json")
 	require.NoError(t, err)
 	require.NotEmpty(t, classHashHex)
+	require.NotEmpty(t, transactionHashHex)
+}
+
+// Before running protostar tests make sure that
+// * protostar is installed
+// * starknet-devnet is running  on http://127.0.0.1:5050 with seed 42
+func TestDeploy(t *testing.T) {
+	// Checking whether protostar is installed
+	cmd := exec.Command("command", "-v", "protostar")
+	if err := cmd.Run(); err != nil {
+		t.Skip("protostar is not installed")
+	}
+
+	// Checking whether starknet-devnet is running on port 5050
+	_, err := http.Get("http://127.0.0.1:5050/is_alive")
+	if err != nil {
+		t.Skip("starknet-devnet is not running")
+	}
+
+	// Creating temporary file with private key
+	tmpFile, err := os.CreateTemp(".", "seed_42_private_key")
+	if err != nil {
+		t.Fatal(fmt.Errorf("could not create temporary file for private key: %w", err))
+	}
+	tmpFile.Write([]byte("0xbdd640fb06671ad11c80317fa3b1799d"))
+	tmpFile.Close()
+	defer os.Remove(tmpFile.Name())
+
+	// Setting up the protostar config
+	conf := config.DefaultProtostarConfig()
+	conf.PrivateKeyPath = tmpFile.Name()
+	conf.AccountAddress = "0x347be35996a21f6bf0623e75dbce52baba918ad5ae8d83b6f416045ab22961a"
+
+	// Calling the Declare to get the class hash for the Deploy function
+	ch, th, err := Declare(conf, "../cairo/build/main.json")
+	require.NoError(t, err)
+	require.NotEmpty(t, ch)
+	require.NotEmpty(t, th)
+
+	// Testing the Deploy function
+	contractAddressHex, transactionHashHex, err := Deploy(conf, ch)
+	require.NoError(t, err)
+	require.NotEmpty(t, contractAddressHex)
 	require.NotEmpty(t, transactionHashHex)
 }
